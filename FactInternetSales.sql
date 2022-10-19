f69ed99a45f0b7c7bd3f5ab0b7c2adf10ea11418
Peter Bühle <peter.buehle@gmail.com>
2022-10-19 11:00:35 +0200

    changed delete line pbu

--- FactInternetSales.sql
+++ FactInternetSales.sql
@@ -16,7 +16,6 @@ CREATE TABLE [dbo].[FactInternetSales](
 	[UnitPriceDiscountPct] [float] NOT NULL,
 	[DiscountAmount] [float] NOT NULL,
 	[ProductStandardCost] [money] NOT NULL,
-	[TotalProductCost] [money] NOT NULL,
 	[SalesAmount] [money] NOT NULL,
 	[TaxAmt] [money] NOT NULL,
 	[Freight] [money] NOT NULL,
